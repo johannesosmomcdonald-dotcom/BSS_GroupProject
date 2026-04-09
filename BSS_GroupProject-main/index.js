6f73cd3cb7c0602cdd6338bcb879c633365139cb
johannesosmomcdonald-dotcom <johannesosmomcdonald@gmail.com>
2026-04-09 19:59:25 +0100

    Put in some html code

---
+++ BSS_GroupProject-main/index.js
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Code for connecting neon, render and github
+
+const express = require("express");
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000; //Port
+
+
+
+// PostgreSQL / Neon connection with SSL
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, 
+});
+
+// Test DB connection
+pool.connect()
+  .catch(err => console.error("Database connection error:", err));
+
+// Root endpoint
+app.get("/", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users;"); //Query for database
+    res.json(rows); //prints results of JSON
+  } catch (error) { // no valid status found
+    console.error("Failed to fetch data:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+///////////////////////////////////////////////////////////////////////////////////////////////
