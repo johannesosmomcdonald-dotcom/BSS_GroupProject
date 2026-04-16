cd182c14d91534a6775ee0a260ea23027921e363
johannesosmomcdonald-dotcom <johannesosmomcdonald@gmail.com>
2026-04-16 17:14:44 +0100

    more syntax editing

--- public/sign_up.js
+++ public/sign_up.js
@@ -31,7 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
             !subject ||
             !degree_type ||
             !email ||
-            !description ||
             !password
         ) {
             alert("Please fill in all required fields.");
