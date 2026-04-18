9a10fcbee78b0500464ddbd48b435e2cf4bef60a
johannesosmomcdonald-dotcom <johannesosmomcdonald@gmail.com>
2026-04-18 17:54:00 +0100

    syntax fix

--- public/login.js
+++ public/login.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const response = await fetch("/login", {
         method: "POST",
         headers: {
-          "Content-Type": "app/json"
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({ email, password })
       });
