4ab69c2fad5396830c8c99ccc2d3a2732f1a05f0
johannesosmomcdonald-dotcom <johannesosmomcdonald@gmail.com>
2026-05-10 22:30:11 +0100

    log in fix

--- public/login.js
+++ public/login.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("#login_Inputs"); // code for selecting specific queries in login.html form
   const message = document.querySelector("#message");
-<<<<<<< HEAD
-=======
   const backBtn = document.querySelector("#backBtn"); // code for backbutton
 
   // loading gif variables
   const divForm = document.querySelector("#divForm");
   const divLoading = document.querySelector("#divLoading");
   const divSuccess = document.querySelector("#divSuccess");
->>>>>>> 143a869 (merging a mix of Hannahs and Toms work)
 
   form.addEventListener("submit", async (event) => { // event listener aka button click 
     event.preventDefault();
@@ -19,13 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     message.textContent = "";
 
-<<<<<<< HEAD
-=======
     // start loading gif
     if (divForm) divForm.style.display = 'none';
     if (divLoading) divLoading.style.display = 'block';
 
->>>>>>> 143a869 (merging a mix of Hannahs and Toms work)
     try { // inputting login 
       const response = await fetch("/login", {
         method: "POST",
@@ -42,10 +36,6 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
       }
 
-<<<<<<< HEAD
-      window.location.href = "/dashboard.html"; // moving to dahsboard if succesful 
-    } catch (error) {
-=======
       // successful loading gif
       if (divLoading) divLoading.style.display = 'none';
       if (divSuccess) divSuccess.style.display = 'block';
@@ -60,18 +50,13 @@ document.addEventListener("DOMContentLoaded", () => {
       if (divLoading) divLoading.style.display = 'none';
       if (divForm) divForm.style.display = 'block';
 
->>>>>>> 143a869 (merging a mix of Hannahs and Toms work)
       message.textContent = error.message;
       console.error("Login error:", error);
     }
   });
 
   backBtn.addEventListener("click", () => { // code for backbutton
-<<<<<<< HEAD
-    window.location.href = "/index.html";
-=======
     window.location.href = "index.html";
->>>>>>> 143a869 (merging a mix of Hannahs and Toms work)
   });
 
 });
