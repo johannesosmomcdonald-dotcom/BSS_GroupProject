e4c8131a22c493736ae1bc8a3d86bbea8d4e1b46
tjmwest <thwe2005@gmail.com>
2026-05-10 22:06:12 +0100

    second attempt at getting verfication to work

--- server.js
+++ server.js
@@ -23,11 +23,19 @@ const pgSession = require("connect-pg-simple")(session); // imports connect-pg-s
 // an email delivery platform which will be used for our email verfication system
 // in both the sign in and log in.
 
-const { Resend } = require("resend"); // imports resend allowing for email verfication
+//const { Resend } = require("resend"); // imports resend allowing for email verfication
 //const resend = new Resend(process.env.RESEND_KEY);// loads our protected data that being a key provided by Resend which will be used to authenticate our requests
 
 const crypto = require("crypto"); // imports crypto which will be used to create tokens for email verfication
-const resend = new Resend(process.env.RESEND_API_KEY);// loads our protected data that being a key provided by Resend which will be used to authenticate our requests
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 ///////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////
